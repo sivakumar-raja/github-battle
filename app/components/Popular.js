1c8947025e6f7a9925462c4fc13a3dd8714a5cf5
Sivakumar Raja <sivaraj2205@gmail.com>
2017-07-14 20:54:14 +0530

    Refactored the Popular component a declarative one using stateless functional components.

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,4 +1,33 @@
 var React = require('react');
+var PropTypes = require('prop-types');
+
+function Language(props) {
+  var languages = ['All', 'JavaScript', 'Java', 'Python', 'CSS', 'Ruby'];
+
+  return(
+    <div>
+      <ul className='languages'>
+        {
+          languages.map(function(lang) {
+            return (
+              <li
+                style={props.selectedLanguage === lang? {color:'red'}: null}
+                onClick={props.onSelect.bind(null, lang)}
+                key={lang}>
+                {lang}
+              </li>
+            );
+        })
+      }
+      </ul>
+    </div>
+  )
+}
+
+Language.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired,
+}
 
 class Popular extends React.Component {
 
@@ -19,24 +48,12 @@ class Popular extends React.Component {
 	}
 
 	render() {
-		var languages = ['All', 'JavaScript', 'Java', 'Python', 'CSS', 'Ruby'];
-
 		return (
 			<div>
-				<ul className='languages'>
-					{
-						languages.map(function(lang) {
-							return (
-								<li
-									style={this.state.selectedLanguage === lang? {color:'red'}: null}
-									onClick={this.updateLanguage.bind(null, lang)}
-									key={lang}>
-									{lang}
-								</li>
-							);
-					}, this)
-				}
-				</ul>
+				<Language
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage}
+        />
 			</div>
 		)
 	}
