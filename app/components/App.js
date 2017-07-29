f85161d71d7bb976eaf2963fafa04d3a0313a079
Sivakumar Raja <sivaraj2205@gmail.com>
2017-07-29 15:27:12 +0530

    Router has been incorporated to navigate between pages.

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,28 @@
 var React = require('react');
 var Popular = require('./Popular');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Nav = require('./Nav');
+var Home = require('./Home');
+var Battle = require('./Battle');
+var Switch = ReactRouter.Switch;
+var NotFound = require('./NotFound');
 
 class App extends React.Component {
 	render() {
 		return (
-			<Popular />
+			<Router>
+				<div className='container'>
+					<Nav />
+					<Switch>
+						<Route exact path='/' component={Home} />
+						<Route path='/battle' component={Battle} />
+						<Route path='/popular' component={Popular} />
+						<Route component={NotFound} />
+					</Switch>
+				</div>
+			</Router>
 		)
 	}
 }
---
+++ app/components/NotFound.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+function NotFound() {
+	return (
+			<div>Not Found!</div>
+	)
+}
+
+module.exports = NotFound;
