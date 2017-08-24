040ad5e94a0c9b4c2d3476edeae2f9cdfbb526a4
Sivakumar <sivakumar.r@kritilabs.com>
2017-08-24 18:48:49 +0530

    Fixed bug, on getting player computation.

--- app/components/BattleResult.js
+++ app/components/BattleResult.js
@@ -1,11 +1,47 @@
 var React = require('react');
-
+var Api = require('../utils/Api')
+var queryString = require('query-string');
 
 class BattleResult extends React.Component {
 
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			winner: null,
+			loser: null
+		}
+	}
+
+	componentDidMount() {
+		var search = queryString.parse(location.search);
+		var players = [];
+		players.push(search.playerOneName);
+		players.push(search.playerTwoName);
+
+
+		Api.battle(players).then(function(respose){
+			this.setState(function() {
+				return {
+					winner : respose[0],
+					loser: respose[1]
+				}
+			});
+		}.bind(this));
+	}
+
 	render() {
 		return (
-			<div>Battle Result </div>
+			<div className='row'>
+				<div>
+					<h2> Winner </h2>
+					{this.state.winner !== null ? <h3> {this.state.winner.profile.name} </h3> : null}
+				</div>
+				<div>
+					<h2> Loser </h2>
+					{this.state.loser !== null ? <h3> {this.state.loser.profile.name} </h3> : null}
+				</div>
+			</div>
 		)
 	}
 }
--- app/utils/Api.js
+++ app/utils/Api.js
@@ -1,6 +1,70 @@
 var axios = require('axios');
 
-module.exports = {
+
+function getProfile(username) {
+	var profileUri = 'https://api.github.com/users/'+username;
+	return axios.get(profileUri)
+		.then(function(response) {
+			return response.data;
+		});
+}
+
+function getRepos(username) {
+	var repoUri = 'https://api.github.com/users/'+username+'/repos';
+	return axios.get(repoUri)
+		.then(function(response) {
+			return response;
+		});	
+}
+
+function calculateStarCount(repos) {
+	return repos.data.reduce(function(count, repo) {
+		return repo.stargazers_count + count;
+	}, 0)
+}
+
+function calculateScore(profile, repos) {
+	var followers = profile.followers;
+	var totalStars = calculateStarCount(repos);
+
+	return (followers * 3) + totalStars;
+}
+
+function handleError(error) {
+	console.warn(error);
+	return null;
+}
+
+
+function getUserData(player) {
+	return axios.all([
+			getProfile(player),
+			getRepos(player)
+		]).then(function (response) {
+			var profile = response[0];
+			var repos = response[1];
+
+			return {
+				profile : profile,
+				score : calculateScore(profile, repos)	
+			}
+		}); 
+}
+
+function sortPlayers (players) {
+   return players.sort(function (a,b) {
+     	return b.score - a.score;
+   });
+ }
+
+module.exports = {	
+
+	battle: function(players) {
+		return axios.all(players.map(getUserData))
+			.then(sortPlayers)
+			.catch(handleError);
+	},
+
    fetchPopularRepos: function(language) {
        var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+
        language +'&sort=stars&order=desc&type=Repositories');
